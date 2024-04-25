19b8ad1dab859e7b9f9c73b2274495964f39fc47
Alan <soldier4443@gmail.com>
2024-04-25 17:07:53 +0900

    add Sum()

--- basic/adder.go
+++ basic/adder.go
@@ -3,3 +3,11 @@ package basic
 func Add(a, b int) int {
 	return a + b
 }
+
+func Sum(numbers []int) (result int) {
+	for _, number := range numbers {
+		result += number
+	}
+
+	return
+}
--- basic/adder_test.go
+++ basic/adder_test.go
@@ -19,3 +19,14 @@ func ExampleAdd() {
 	fmt.Println(sum)
 	// Output: 6
 }
+
+func TestSum(t *testing.T) {
+
+	numbers := []int{1, 2, 3, 4, 5}
+	sum := Sum(numbers)
+	expected := 15
+
+	if sum != expected {
+		t.Errorf("expected '%d' but got '%d'", expected, sum)
+	}
+}
