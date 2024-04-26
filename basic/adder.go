f6d86e56118e8ed77a690d54a59891528d96dd48
Alan <soldier4443@gmail.com>
2024-04-26 15:20:39 +0900

    add SumAll() function

--- basic/adder.go
+++ basic/adder.go
@@ -11,3 +11,29 @@ func Sum(numbers []int) (result int) {
 
 	return
 }
+
+func SumAll(listOfNumbers ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range listOfNumbers {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
+
+func SumAllTrails(listOfNumbers ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range listOfNumbers {
+		lenOfNumbers := len(numbers)
+		if lenOfNumbers == 0 {
+			sums = append(sums, 0)
+		} else {
+			tails := numbers[1:]
+			sums = append(sums, Sum(tails))
+		}
+	}
+
+	return sums
+}
--- basic/adder_test.go
+++ basic/adder_test.go
@@ -2,6 +2,7 @@ package basic
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 )
 
@@ -21,7 +22,6 @@ func ExampleAdd() {
 }
 
 func TestSum(t *testing.T) {
-
 	numbers := []int{1, 2, 3, 4, 5}
 	sum := Sum(numbers)
 	expected := 15
@@ -29,4 +29,36 @@ func TestSum(t *testing.T) {
 	if sum != expected {
 		t.Errorf("expected '%d' but got '%d'", expected, sum)
 	}
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{4, 5}, []int{3, 7})
+	want := []int{9, 10}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func TestSumAllTrails(t *testing.T) {
+	checkSums := func(got []int, want []int, t *testing.T) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTrails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+
+		checkSums(got, want, t)
+	})
+
+	t.Run("safely handle empty slices", func(t *testing.T) {
+		got := SumAllTrails([]int{}, []int{1, 2, 3})
+		want := []int{0, 5}
+
+		checkSums(got, want, t)
+	})
 }
